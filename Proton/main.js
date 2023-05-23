1af51520c1f16552488c0d95b60b0aef8febb564
OneCodeVector <117361107+OneCodeVector@users.noreply.github.com>
2023-05-23 12:21:41 -0400

    Upd1
    Code readability improvements, and setup for scene implementation!

--- Proton/main.js
+++ Proton/main.js
@@ -1,4 +1,49 @@
- var PointCount = 0
+var PointCount = 0
+
+class __ThreeDimensionalVec3 {
+    constructor (X, Y, Z) {
+        this.X = X
+        this.Y = Y
+        this.Z = Z
+    }
+
+    scale(Num) {
+        return new Vec3(this.X*Num, this.Y*Num, this.Z*Num)
+    }
+
+    add(Vector) {
+        return new Vec3(this.X+Vector.X, this.Y+Vector.Y, this.Z+Vector.Z)
+    }
+
+    cross(Vector) {
+        return new Vec3(this.Y * Vector.Z - this.Z * Vector.Y, this.Z * Vector.X - this.X * Vector.Z, this.X * Vector.Y - this.Y * Vector.X)
+    }
+
+    dotProduct(Vector) {
+        return this.X*Vector.X + this.Y*Vector.Y + this.Z*Vector.Z
+    }
+
+    normalize() {
+        let Mag = sqrt(this.X*this.X + this.Y*this.Y + this.Z*this.Z)
+        return new Vec3(this.X/Mag, this.Y/Mag, this.Z/Mag)
+    }
+
+    magnitude() {
+        return sqrt(this.X*this.X + this.Y*this.Y + this.Z*this.Z)
+    }
+
+    rotate(Axis, Angle) {
+        let Vector = new Vec3(this.X, this.Y, this.Z)
+        let Mag = sqrt(Axis.X*Axis.X + Axis.Y*Axis.Y + Axis.Z*Axis.Z)
+        let Norm = new Vec3(Axis.X/Mag, Axis.Y/Mag, Axis.Z/Mag)
+        
+        Vector.scale(cos(Angle)) .add(Norm.cross(Vector).scale(sin(Angle))) .add(Norm.scale(Norm.dotProduct(Vector)).scale(1-cos(Angle)))
+    }
+
+    project() {
+        return new Vec2((this.X - Camera.X) / (this.Z * tan((Camera.Fov*PI) / 180)), (this.Y - Camera.Y) / (this.Z * tan((Camera.Fov*PI) / 180)))
+    }
+}
 
 class __TwoDimensionalVec2 {
     constructor (X, Y) {
@@ -30,7 +75,20 @@ class __TwoDimensionalVec2 {
     }
 }
 
+class __ThreeDimensionalVec2 {
+    constructor (X, Y) {
+        this.X = X
+        this.Y = Y
+        this.Type = "Vector2D"
+        this.ExpressionID = "Point"+PointCount
+        PointCount ++
+    }
+
+    
+}
+
 var Vec2 = __TwoDimensionalVec2
+var Vec3 = undefined
 
 var Proton = {
     __TickData: { // tick time not made yet 
@@ -47,48 +105,38 @@ var Proton = {
         RotationZ: 0,
     },
     
-    plotFunction: (Equation, Accuracy) => {
-        var Max = 10*Accuracy
-        var EquationLatex = `(-10,${Equation(-Max)})`
-        
-        for (let X = (-Max)+1; X < Max; X++) {
-            EquationLatex += `,(${X/Accuracy},${Equation(X/Accuracy)})`
-        }
-        Calc.setExpression({
-            type: "expression",
-            latex: EquationLatex,
-        })
-    },
-
-    Point: (X, Y) => {
-        
-    },
-
-    setMode: (Mode) => {
-        if (Mode.toLowerCase() == "3D") {
-            Proton.__Mode = "3D"
-        }
-    },
-
-    on: (Event, Function) => {
-        switch ( Event) {
-            case ("Tick") : { // more soon
-                Proton.__TickFunction = Function
-                break
-            }
-        }
-    },
-    
     tick: undefined,
+}
+Proton.plotFunction = (Equation, Accuracy) => {
+    var Max = 10*Accuracy
+    var EquationLatex = `(-10,${Equation(-Max)})`
+    
+    for (let X = (-Max)+1; X < Max; X++) {
+        EquationLatex += `,(${X/Accuracy},${Equation(X/Accuracy)})`
+    }
+    Calc.setExpression({
+        type: "expression",
+        latex: EquationLatex,
+    })
+}
 
-    start: () => {
-        Proton.__Ticking = true
-        requestAnimationFrame(Proton.tick)
-    },
+Proton.Point = (X, Y) => {
+    
+}
 
-    pause: () => {
-        Proton.__Ticking = false
-    },
+Proton.setMode = (Mode) => {
+    if (Mode.toLowerCase() == "3D") {
+        Proton.__Mode = "3D"
+    }
+}
+
+Proton.on = (Event, Function) => {
+    switch ( Event) {
+        case ("Tick") : { // more soon
+            Proton.__TickFunction = Function
+            break
+        }
+    }
 }
 
 Proton.tick = () => {
@@ -97,4 +145,18 @@ Proton.tick = () => {
     if (Proton.__Ticking) {
         requestAnimationFrame(Proton.tick)
     }
+}
+
+Proton.start = () => {
+    Proton.__Ticking = true
+    requestAnimationFrame(Proton.tick)
+}
+
+Proton.pause = () => {
+    Proton.__Ticking = false
+}
+
+Proton.createScene = (Type, Settings) => {
+    Vec2 = __ThreeDimensionalVec2
+    Vec3 = __ThreeDimensionalVec3
 }
